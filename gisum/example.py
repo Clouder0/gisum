a6f2080bf606e673ed38f540d43cea8289b0e61f
Clouder0 <clouder0@outlook.com>
2023-11-20 23:30:38 +0800

    init

---
+++ gisum/example.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+def add(a, b):
+    return a + b
---
+++ tests/test_example.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+import gisum.example
+
+def test_add():
+    assert gisum.example.add(1, 2) == 3
