2766c7d006dafe61f5254f953ec726260b31d4fd
Clouder0 <clouder0@outlook.com>
2023-11-27 23:55:03 +0800

    Basic summarize
    	modified:   .env.example
    	modified:   .gitignore
    	modified:   gisum/config.py
    	modified:   gisum/lib.py
    	new file:   gisum/main.py

--- gisum/config.py
+++ gisum/config.py
@@ -1,6 +1,16 @@
-from dotenv import dotenv_values
+from dataclasses import dataclass
+from dotenv import dotenv_values, load_dotenv
 
-config = {
-    **dotenv_values(".env")
-}
+env_values = dotenv_values(".env")
 
+
+@dataclass
+class Config:
+    api_key: str
+    base_url: str
+
+
+config = Config(
+    api_key=env_values["API_KEY"],
+    base_url=env_values.get("BASE_URL", "https://api.openai.com"),
+)
